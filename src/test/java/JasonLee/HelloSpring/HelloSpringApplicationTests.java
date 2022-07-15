87afb4188233bd54a86d66552afe3fb9dede7662
Jason Sejik Lee <gggabikr@gmail.com>
2022-07-16 00:48:09 +0900

    Initial commit

---
+++ src/test/java/JasonLee/HelloSpring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package JasonLee.HelloSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
