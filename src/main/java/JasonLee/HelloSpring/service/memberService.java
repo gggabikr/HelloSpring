850255b4e0be4f388700c0187a1162aac134f24b
Jason Sejik Lee <gggabikr@gmail.com>
2022-07-20 20:20:31 +0900

    Create memberService.java

---
+++ src/main/java/JasonLee/HelloSpring/service/memberService.java
@@ -0,0 +1,44 @@
+package JasonLee.HelloSpring.service;
+
+import JasonLee.HelloSpring.domain.Member;
+import JasonLee.HelloSpring.repository.MemberRepository;
+import JasonLee.HelloSpring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class memberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member){
+        //같은 이름이있는 중복 회원X
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+        //result값이 있으면 해당 오류 발생. result가 Optional이기때문에 가능한 방법.
+        //Optional이 아니면 if(result == !null)~~ 이렇게 가야한다.
+
+        //그러나 굳이 result로 할필요없이 바로 하는것도 가능.
+        validateDuplicateMember(member);//중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
