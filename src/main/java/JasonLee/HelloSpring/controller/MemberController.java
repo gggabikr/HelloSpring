432401bf438451fe07b14c3b7cca4b400b31a832
Jason Sejik Lee <gggabikr@gmail.com>
2022-07-21 22:44:17 +0900

    Update MemberController.java

--- src/main/java/JasonLee/HelloSpring/controller/MemberController.java
+++ src/main/java/JasonLee/HelloSpring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package JasonLee.HelloSpring.controller;
 
+import JasonLee.HelloSpring.domain.Member;
 import JasonLee.HelloSpring.service.memberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -17,5 +23,27 @@ public class MemberController {
     //이전 테스트에서는 개발자가 직접 주입했고, 여기서는 @Autowired에 의해 스프링이 주입해준다.
     public MemberController(JasonLee.HelloSpring.service.memberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+        //redirect to 경로 '/' = 홈화면으로 보내겠다는 소리
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
