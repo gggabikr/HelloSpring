16796938effa720ecbe8ce7cd312635fd62e2c03
Jason Sejik Lee <gggabikr@gmail.com>
2022-07-21 22:44:25 +0900

    Update MemoryMemberRepository.java

--- src/main/java/JasonLee/HelloSpring/repository/MemoryMemberRepository.java
+++ src/main/java/JasonLee/HelloSpring/repository/MemoryMemberRepository.java
@@ -1,11 +1,9 @@
 package JasonLee.HelloSpring.repository;
 
 import JasonLee.HelloSpring.domain.Member;
-import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
