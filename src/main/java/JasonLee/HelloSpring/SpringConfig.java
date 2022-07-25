7775f3e9492125e8c4ad33327275580e4a4a0aa1
Jason Sejik Lee <gggabikr@gmail.com>
2022-07-26 01:38:17 +0900

    Update SpringConfig.java

--- src/main/java/JasonLee/HelloSpring/SpringConfig.java
+++ src/main/java/JasonLee/HelloSpring/SpringConfig.java
@@ -1,13 +1,25 @@
 package JasonLee.HelloSpring;
 
+import JasonLee.HelloSpring.repository.JdbcMemberRepository;
 import JasonLee.HelloSpring.repository.MemberRepository;
 import JasonLee.HelloSpring.repository.MemoryMemberRepository;
 import JasonLee.HelloSpring.service.memberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public memberService memberService(){
         return new memberService(memberRepository());
@@ -15,6 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+
     }
 }
