f08667db5374512dc7a29716768cd78141fcbc9a
Jason Sejik Lee <gggabikr@gmail.com>
2022-07-31 20:10:17 +0900

    Update SpringConfig.java

--- src/main/java/JasonLee/HelloSpring/SpringConfig.java
+++ src/main/java/JasonLee/HelloSpring/SpringConfig.java
@@ -19,24 +19,33 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    EntityManager em;
+
+    //    EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+//
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
 
     @Bean
     public memberService memberService(){
-        return new memberService(memberRepository());
+        return new memberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//
+//    }
 }
