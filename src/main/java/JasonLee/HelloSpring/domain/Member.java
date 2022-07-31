7ec4c6d1de1b1ee7100878389173dcde99db4bae
Jason Sejik Lee <gggabikr@gmail.com>
2022-07-31 18:33:09 +0900

    Update Member.java

--- src/main/java/JasonLee/HelloSpring/domain/Member.java
+++ src/main/java/JasonLee/HelloSpring/domain/Member.java
@@ -1,7 +1,16 @@
 package JasonLee.HelloSpring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//    @Column(name = "Name")
+    //해당 값이 들어가는 컬럼 명이 UserName이면 뒤쪽 따옴표안에 그렇게 쓰면 된다.
+    //해당 데이터가 들어가는 컬럼 이름을 쓰는 것.
     private String name;
 
     public String getName() {
